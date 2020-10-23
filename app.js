857437e761bf48b9bdc8ec43438ddfab231456e9
Chaegeon <thscorjs1002@naver.com>
2020-10-23 20:19:31 +0900

    Finish

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const passport = require('passport');
 dotenv.config();
 const pageRouter = require('./routes/page');
 const authRouter = require('./routes/auth');
+const postRouter = require('./routes/post');
+const userRouter = require('./routes/user');
 const { sequelize } = require('./models');
 const passportConfig = require('./passport');
 
@@ -32,6 +34,7 @@ sequelize.sync({ force: false })
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/img', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -49,6 +52,8 @@ app.use(passport.session());
 
 app.use('/', pageRouter);
 app.use('/auth', authRouter);
+app.use('/post', postRouter);
+app.use('/user', userRouter);
 
 app.use((req, res, next) => {
   const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
--- routes/page.js
+++ routes/page.js
@@ -20,28 +20,23 @@ router.get('/join', isNotLoggedIn, (req, res) => {
   res.render('join', { title: '회원가입 - NodeBird' });
 });
 
-router.get('/', (req, res, next) => {
-  Post.findAll({
+router.get('/', async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
       include: {
-          model: User,
-          attributes: ['id', 'nick'],
+        model: User,
+        attributes: ['id', 'nick'],
       },
       order: [['createdAt', 'DESC']],
-  })
-  .then((posts) => {
-      res.render('main', {
-          title: 'NodeBird',
-          twits: posts,
-          user: req.user,
-          loginError: req.flash('loginError'),
-      });
-  })
-  .catch((error) => {
-      console.error(error);
-      next(error);
-  });
+    });
+    res.render('main', {
+      title: 'NodeBird',
+      twits: posts,
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 });
-
-module.exports = router;
 
 module.exports = router;
