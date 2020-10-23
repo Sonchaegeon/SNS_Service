8d701c50f3136764027e033a0980302331ad0e72
Chaegeon <thscorjs1002@naver.com>
2020-10-23 17:05:23 +0900

    KakaoStrategy

--- routes/auth.js
+++ routes/auth.js
@@ -52,4 +52,13 @@ router.get('/logout', isLoggedIn, (req, res) => {
     req.session.destroy();
     req.redirect('/');
 });
+
+router.get('/kakao', passport.authenticate('kakao'));
+
+router.get('/kakao/callback', passport.authenticate('kakao', {
+    failureRedirect: '/',
+}), (req, res) => {
+    res.redirect('/');
+});
+
 module.exports = router;
